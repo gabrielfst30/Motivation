b99eb6dbf182c2825244a8481ca0aec6afd95f73
Gabriel Santa Ritta <gabrielfst30@gmail.com>
2022-10-10 23:17:25 -0300

    feature: criando uma constande para a key do sh-pref

---
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -0,0 +1,11 @@
+package com.example.motivation.infra
+
+//criando uma const para o nome da key
+class MotivationConstants private constructor(){//private constructor para não ter como instanciar
+
+    object KEY{
+       const val USER_NAME = "USER_NAME"
+
+    }
+
+}
