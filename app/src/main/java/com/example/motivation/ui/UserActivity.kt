347eaa0c3bb2fbb5eae002354604e121dda34c42
Gabriel Santa Ritta <gabrielfst30@gmail.com>
2022-10-10 23:18:33 -0300

    feature: verificando usuário

--- app/src/main/java/com/example/motivation/ui/UserActivity.kt
+++ app/src/main/java/com/example/motivation/ui/UserActivity.kt
@@ -1,10 +1,13 @@
-package com.example.motivation
+package com.example.motivation.ui
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPrefences
 import com.example.motivation.databinding.ActivityUserBinding
 
 
@@ -17,26 +20,41 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //escodendo barra
+        //ESCONDENDO BARRA
         supportActionBar?.hide()
 
         binding.buttonSave.setOnClickListener(this)
 
+        verifyUserName()
+
     }
+    //VERIFICANDO SE O NOME DO USUÁRIO JA FOI DEFINIDO
+    private fun verifyUserName() {
+        val name = SecurityPrefences(this).getString(MotivationConstants.KEY.USER_NAME)
+        if(name != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
 
     override fun onClick(v: View) {
         if (v.id == R.id.button_save) {
             handleSave()//metodo para salvar
 
         }
-
     }
 
     private fun handleSave() {
+
         val name = binding.editName.text.toString() //pegando o texto inserido no editText
 
         //validação de input vazio
         if (name != "") {
+
+            //instanciando o sh preferences para salvar o nome
+            SecurityPrefences(this).storeString(MotivationConstants.KEY.USER_NAME, name)
 
             // indo para outra activity caso o nome esteja preenchido
             startActivity(Intent(this, MainActivity::class.java))
